70a84bc5ef50bdcc906a225dcb4e39a1879b5a5e
matsu <rice_shower_35@hotmail.com>
2022-04-21 18:02:52 +0900

    riverpod練習

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 // import 'dart:js';
 
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final appNameProvider = Provider((ref) => 'Flutter Test');
-final counterProvider = StateProvider((ref) => 0);
+// final counterProvider = StateProvider((ref) => 0);
 
 void main() {
   runApp(const ProviderScope(
@@ -27,44 +29,61 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// 状態管理表示部分（Model側）を作る
+final counterProvider = ChangeNotifierProvider<CountModel>(
+  (ref) => CountModel(),
+);
+
+class CountModel extends ChangeNotifier {
+  int state = 0;
+
+  void incrementCounter() {
+    state++;
+    notifyListeners();
+  }
+}
+
 class _MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // final _counter = ref.watch(counterProvider);
+    // Modelを読み込み
+    // final countModel = context.read(counterProvider);
     return Scaffold(
-        appBar: AppBar(
-          title: Consumer(builder: (context, ref, _) {
-            final appName = ref.watch(appNameProvider);
-            return Text(appName);
-          }),
+      appBar: AppBar(
+        title: Consumer(builder: (context, ref, _) {
+          final appName = ref.watch(appNameProvider);
+          return Text(appName);
+        }),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Consumer(builder: (context, ref, _) {
+              final _counter = ref.watch(counterProvider);
+              return Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headline4,
+              );
+            }),
+          ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              Consumer(builder: (context, ref, _) {
-                final _counter = ref.watch(counterProvider.state);
-                return Text(
-                  '$_counter',
-                  style: Theme.of(context).textTheme.headline4,
-                );
-              }),
-            ],
-          ),
-        ),
-        floatingActionButton: Consumer(builder: (context, ref, _) {
-          return FloatingActionButton(
-            onPressed: () {
-              ref.watch(counterProvider.state).state++;
-            },
-            tooltip: 'Increment',
-            child: const Icon(Icons.add),
-          );
-        })
-        // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+      floatingActionButton: Consumer(builder: (context, ref, _) {
+        final countModel = ref.watch(counterProvider);
+        return FloatingActionButton(
+          onPressed: () {
+            countModel.incrementCounter();
+          },
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
         );
+      }),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
   }
 }
